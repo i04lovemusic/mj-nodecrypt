c6430f8e6339ea91f9836bf4cc427fbba99894dc
i04lovemusic <51805831+i04lovemusic@users.noreply.github.com>
2025-12-18 06:13:00 +0800

    Update UI and help text for localization

--- client/js/util.i18n.js
+++ client/js/util.i18n.js
@@ -142,7 +142,7 @@ const LANGUAGES = {
 			'meta.twitter_description': 'NodeCrypt 是一个端到端加密的开源聊天系统，所有加密解密均在客户端本地完成，服务器无法获取明文。',
 			
 			// Login and main UI
-			'ui.enter_node': '进入新的节点',
+			'ui.enter_node': '言出如风，过耳成空。',
 			'ui.username': '用户名',
 			'ui.node_name': '节点名称',
 			'ui.node_password': '节点密码',
@@ -221,7 +221,7 @@ const LANGUAGES = {
 			'help.title': '使用说明',
 			'help.back_to_login': '返回登录',
 			'help.usage_guide': '使用说明',
-			'help.what_is_nodecrypt': '🔐 什么是无痕加密聊天系统？',			'help.what_is_nodecrypt_desc': '无痕加密聊天系统是一个真正的端到端加密聊天系统。采用无数据库架构，所有消息在您的设备上本地加密，服务器仅作为加密数据的中转站，无法获取您的任何明文内容。',
+			'help.what_is_nodecrypt': '🔐 什么是无痕之语？',			'help.what_is_nodecrypt_desc': '无痕之语是一个真正的端到端加密聊天系统。采用无数据库架构，所有消息在您的设备上本地加密，服务器仅作为加密数据的中转站，无法获取您的任何明文内容。',
 			'help.how_to_start': '🚀 快速开始',
 			'help.step_username': '输入用户名',
 			'help.step_username_desc': '选择一个在房间中显示的昵称，可以是任何您喜欢的名称',
